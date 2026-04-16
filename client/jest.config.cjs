c7844b7228265782851dcf8546b2c4aa906b31eb
Giteshhaudhari <giteshchaudhari681@gmail.com>
2026-04-16 13:10:50 +0530

    Revert "[codex] Add CI workflow"

--- client/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  moduleFileExtensions: ['js', 'jsx'],
-  testMatch: ['<rootDir>/src/**/*.test.jsx'],
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-};
